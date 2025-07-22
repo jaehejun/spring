dcf1d5ae49a31af1aa66ec853679b3d12f8e3475
jaehee <wogml414@gmail.com>
2025-07-22 15:56:48 +0900

    application.yml

--- src/test/java/com/example/springdemo/SpringDemoApplicationTests.java
+++ src/test/java/com/example/springdemo/SpringDemoApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.springdemo;
 
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Log4j2
 @SpringBootTest
 class SpringDemoApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("contextLoad.............");
     }
 
 }
